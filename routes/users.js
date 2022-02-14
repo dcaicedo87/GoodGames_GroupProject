cb6ea64d38c67a3db63d13dfd35b49b07afaccdc
anthony <theanthonyarellano@gmail.com>
2022-02-14 18:24:05 -0500

    started post to /users/register

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,49 @@ var express = require('express');
 var router = express.Router();
 const { asyncHandler, csrfProtection } = require('./utils');
 const db = require('../db/models')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs');
+
+const userValidators = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username')
+    .isLength({ max: 100 })
+    .withMessage('First Name must not be more than 100 characters long.'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email')
+    .isLength({ max: 100 })
+    .withMessage('Email must not be more than 100 characters long.')
+    .isEmail()
+    .withMessage('Email is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email is already in use by another account.');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long.')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password.')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long.')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password.');
+      }
+      return true;
+    }),
+];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -24,5 +67,25 @@ router.get('/register', csrfProtection, (req, res) => {
   });
 });
 
+router.post('/register', csrfProtection, userValidators, asyncHandler(async (req, res) => {
+  const {
+    email,
+    username,
+    password
+  } = req.body;
 
+  const user = db.User.build({
+    email,
+    username
+  });
+
+  const validatorErrors = validationResult(req);
+  if (validatorErrors.isEmpty()) {
+    const hashedPass = await bcrypt.hash(password, 11);
+    user.hashedPass = hashedPass;
+    await user.save();
+    // TODO add login middleware
+    res.redirect('/')
+  }
+}));
 module.exports = router;
