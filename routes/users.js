d7be8c87d0d123b294e898f63c1dfb3f13c88888
anthony <theanthonyarellano@gmail.com>
2022-02-14 17:39:37 -0500

    Set up login pug template and began register route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { asyncHandler, csrfProtection } = require('./utils');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('user-login', {
+    title: "Login",
+    csrfToken: req.csrfToken()
+  });
+});
+
+router.get('/register', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('user-register', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken()
+  });
+});
+
 
 module.exports = router;
