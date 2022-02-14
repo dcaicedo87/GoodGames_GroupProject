54dfd7267bc1ab459b8e28700890d9d7929f1bf7
anthony <theanthonyarellano@gmail.com>
2022-02-14 17:44:53 -0500

    Set up get to /register route along with pug template

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { asyncHandler, csrfProtection } = require('./utils');
+const db = require('../db/models')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
