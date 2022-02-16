762316f3d16d272678627999b9e47c23b88fba6b
anthony <theanthonyarellano@gmail.com>
2022-02-15 20:15:08 -0500

    Progress on review eager loading with user id included

--- routes/games.js
+++ routes/games.js
@@ -4,7 +4,7 @@ const { asyncHandler, csrfProtection } = require("./utils");
 const db = require("../db/models");
 // const Review = require('../db/models')
 const { requireAuth } = require("../auth");
-const { Review } = require('../db/models')
+const { Review, User } = require('../db/models')
 router.get("/", (req, res) => {
   const user = res.locals.user;
   res.render("games-page", { user });
@@ -14,15 +14,24 @@ router.get("/:id", async (req, res) => {
   const id = req.params.id
   const user = res.locals.user;
   const game = await db.Game.findByPk(id, {
-    include: {
-      model: Review,
-      where: {
-        gameId: id
-      }
-    }
+    include:
+      {
+        model: Review,
+        where: {
+          gameId: id
+        },
+        include: {
+          model: User,
+          where: {
+            id
+          }
+        }
+      },
   })
   const reviewContent = game.Reviews[0].content;
-  res.render("game-info", { game, reviewContent })
+  const username = game.Reviews[0].User.username;
+  // const reviewContent = game.Reviews[0].content;
+  res.render("game-info", { game, reviewContent, username })
 });
 
 module.exports = router;
