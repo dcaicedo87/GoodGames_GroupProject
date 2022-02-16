52d94ad9cb993a8dec858fb7a7df860c1482bf2b
MorganGravelat <mgravelat@gmail.com>
2022-02-16 11:34:15 -0500

    Seeds for games setup, games page skeleton finished

--- db/seeders/20220215210604-reviewSeed.js
+++ db/seeders/20220215210604-reviewSeed.js
@@ -5,7 +5,11 @@ module.exports = {
     return queryInterface.bulkInsert(
         "Reviews",
         [
-          { content: "Game Good", userId: 1, gameId: 1, createdAt: new Date(), updatedAt: new Date()},
+          { content: "Game Good", userId: 1, gameId: 3, createdAt: new Date(), updatedAt: new Date()},
+          { content: "WHY THIS GMEA BAD?!", userId: 1, gameId: 4, createdAt: new Date(), updatedAt: new Date()},
+          { content: "Meh?", userId: 1, gameId: 2, createdAt: new Date(), updatedAt: new Date()},
+          { content: "This isn't publix", userId: 1, gameId: 1, createdAt: new Date(), updatedAt: new Date()},
+
         ],
       {}
     );
