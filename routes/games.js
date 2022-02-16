8bd92e2a4dd117b5f76074535ed4154e7c19b637
anthony <theanthonyarellano@gmail.com>
2022-02-15 19:58:00 -0500

    Set up partial gameInfo pug file and began eagar loading to import reviews in game object

--- routes/games.js
+++ routes/games.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
 const db = require("../db/models");
+// const Review = require('../db/models')
 const { requireAuth } = require("../auth");
-
+const { Review } = require('../db/models')
 router.get("/", (req, res) => {
   const user = res.locals.user;
   res.render("games-page", { user });
@@ -12,8 +13,16 @@ router.get("/", (req, res) => {
 router.get("/:id", async (req, res) => {
   const id = req.params.id
   const user = res.locals.user;
-  const game = await db.Game.findByPk(id)
-  res.render("game-info", { game, user })
+  const game = await db.Game.findByPk(id, {
+    include: {
+      model: Review,
+      where: {
+        gameId: id
+      }
+    }
+  })
+  const reviewContent = game.Reviews[0].content;
+  res.render("game-info", { game, reviewContent })
 });
 
 module.exports = router;
