9d87f664fdc291515807aaef9e10d97622fd03f1
anthony <theanthonyarellano@gmail.com>
2022-02-17 16:24:06 -0500

    Progress made on editing comments

--- public/javascripts/game-info.js
+++ public/javascripts/game-info.js
@@ -33,46 +33,59 @@ window.addEventListener("load", async (event) => {
         if (editButtons) {
             editButtons.forEach(editButton => {
                 editButton.addEventListener('click', async (e) => {
+                    editButton.style.display = "none";
                     const id = e.target.id;
                     const realId = id.split('w');
                     const reviewDiv = document.getElementById(id);
                     const children = reviewDiv.childNodes;
-                    const content = children[1];
+                    let content;
+                    if (children.length === 7) {
+                        content = children[3];
+                    } else {
+                        content = children[1];
+                    }
+                    // const content = children[1];
                     const newText = document.createElement('input');
                     newText.innerText = content.innerText;
 
                     reviewDiv.innerHTML += `
                     <div>
                         <textarea id="review-edit-value${id}">${content.innerText}</textarea>
-                        <button id="review-edit-submit-button">Submit Edit</button>
+                        <button class="review-edit-submit-button">Submit Edit</button>
                     </div>
                     `
-                    const editSubmit = document.getElementById('review-edit-submit-button');
-                    editSubmit.addEventListener('click', async (e) => {
-                        let newReview = document.getElementById(`review-edit-value${id}`);
-                        const content = newReview.value;
-                        const data = { content }
-                        console.log(realId[1]);
-                        try {
-                            const res = await fetch(`http://localhost:8080/reviews/${realId[1]}`, {
-                                method: "PUT",
-                                headers: {
-                                    "Content-Type" : "application/json"
-                                },
-                                body: JSON.stringify(data)
-                            });
-                            const response = await res.json();
-                            console.log(response);
-                        } catch (err) {
-                            console.log("Failed to edit review.");
-                        }
-                        children[1].innerHTML = content;
-                        newReview.remove();
-                        editSubmit.remove();
-                        grabEditButton();
-                        grabDeleteButton();
-                    });
+                    const editSubmits = document.querySelectorAll('.review-edit-submit-button');
+                    editSubmits.forEach(editSubmit => {
+                        editSubmit.addEventListener('click', async (e) => {
+                            let newReview = document.getElementById(`review-edit-value${id}`);
+                            const content = newReview.value;
+                            const data = { content }
+                            try {
+                                const res = await fetch(`http://localhost:8080/reviews/${realId[1]}`, {
+                                    method: "PUT",
+                                    headers: {
+                                        "Content-Type" : "application/json"
+                                    },
+                                    body: JSON.stringify(data)
+                                });
+                                const response = await res.json();
+                                console.log(response);
+                            } catch (err) {
+                                console.log("Failed to edit review.");
+                            }
+                            if (children.length === 9) {
+                                children[3].innerHTML = content;
+                            } else {
+                                children[1].innerHTML = content;
+                            }
+                            newReview.remove();
+                            editSubmit.remove();
+                            grabEditButton();
+                            grabDeleteButton();
+                      });
+                   });
                 });
+                editButton.style.display = "inline-block";
             });
         };
     };
@@ -109,29 +122,18 @@ window.addEventListener("load", async (event) => {
         const newHTML = `
        <div id=review${id}>
         <p class="game-info-review-username fontClass">${username}: </p>
-        <p class="game-info-review-content fontClass">${content} </p>
-        <p class="game-info-review-timestamp fontClass">${createdAt} </p>
+        <p class="game-info-review-content fontClass">${content}
+        <p class="game-info-review-timestamp fontClass">${createdAt}
         <button class="game-info-review-delete" id=review${id}>Delete</button>
-        <button class="game-info-review-edit">Edit</button>
+        <button class="game-info-review-edit" id=review${id}>Edit</button>
         <div class="game-info-review-underline"></div>
        </div>
        `;
         const reviewWrapper = document.getElementById("reviewWrapper");
         reviewWrapper.innerHTML += newHTML;
-        // ADD another query selector
         textArea.value = "";
         grabDeleteButton();
         grabEditButton();
     });
 
-    // const editButtons = document.querySelectorAll(".game-info-review-edit");
-    // if (editButtons) {
-    //     editButtons.forEach(editButton => {
-    //         editButton.addEventListener('click', e => {
-    //             // TODO Add fetch to edit from database and display on page
-    //             const id = e.target.id;
-
-    //         });
-    //     });
-    // }
 });
