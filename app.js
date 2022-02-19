433736dd7b8b197bf6df21e5bdb3ff2ac7b18927
anthony <theanthonyarellano@gmail.com>
2022-02-18 20:09:35 -0500

    cors

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const app = express();
 //subtle changes//
 app.set("view engine", "pug");
 
-app.use(cors({ origin: "https://aa-ggames.herokuapp.com/" }));
+app.use(cors({ origin: "*",
+methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
